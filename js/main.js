a5898630fd07e2ed4c133d5469ce68a54b6386ad
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-05-18 22:06:05 +0200

    starter, nav, header, intro

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.element', {
+    strings: ['Web &amp; Mobile development', 'Graphic Design', 'Internet Marketing'],
+    smartBackspace: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity
+});
